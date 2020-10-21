987cc8a12731d4451d3e9b1b6cf84b2c48675e98
GiorgiDolidzee <giorgi.dolidze.2@btu.edu.ge>
2020-10-21 16:16:23 +0400

    first commit

---
+++ app/src/main/java/com/example/lecture3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lecture3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lecture3.R.layout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
